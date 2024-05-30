32c815e7149515408706e93b24ec9c1bfd130bdb
chu <inin7674@naver.com>
2024-05-30 16:36:11 +0900

    스위치 형식 버튼

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,32 @@ import "./App.css";
 function App() {
   const [like, unlike] = useState(false);
   const [hate, unhate] = useState(false);
+  //Like를 누를시 hate가 true 면 false로 바꾸고 like를 true로 바꿈
+  const Like_1 = () => {
+    if(hate === true) {
+      unhate(false)
+    }
+    unlike((좋아요) => !좋아요)}
+    
+  //Hate를 누를시 like가 true 면 false로 바꾸고 hate를 true로 바꿈
+  const Hate_1 = () => {
+    if(like === true) {
+      unlike(false)
+    }
+    unhate((싫어요) => !싫어요)}
 
-  let Like =
-    like === false ? (
-      <button onClick={() => unlike((좋아요) => !좋아요)}>👍🏿</button>
+  const Like =
+    like ? (
+      <button onClick={Like_1}>👍🏿</button>
     ) : (
-      <button onClick={() => unlike((좋아요) => (좋아요 = false))}>👍</button>
+      <button onClick={Like_1}>👍</button>
     );
 
-  let Hate =
-    hate === false ? (
-      <button onClick={() => unhate((싫어요) => !싫어요)}>👎🏿</button>
+  const Hate =
+    hate ? (
+      <button onClick={Hate_1}>👎🏿</button>
     ) : (
-      <button onClick={() => unhate((싫어요) => !싫어요)}>👎</button>
+      <button onClick={Hate_1}>👎</button>
     );
   return (
     <>
